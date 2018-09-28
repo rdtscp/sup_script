4b2a61c6af6101bb802945e57ee4af5901e1e82f
Alexander Wilson <acwilson96@gmail.com>
2018-09-28 22:44:37 +0100

    Refactor content for readibility.

--- content.js
+++ content.js
@@ -2,88 +2,114 @@
  *
  *                 _ _             ___  __ 
  *  __ _ ____ __ _(_) |___ ___ _ _/ _ \/ / 
- * / _` / _\ V  V / | (_-</ _ \ ' \_, / _ \
+ * / _` / _\ V  V / | (_-</ _ \ " \_, / _ \
  * \__,_\__|\_/\_/|_|_/__/\___/_||_/_/\___/
  *                                        
  *
  */
 
-let UKname;
-let UKemail;
-let UKtel;
-let UKaddr1;
-let UKaddr2;
-let UKaddr3;
-let UKcity;
-let UKpostcode;
-let UKcountry;
-let UKcardtype;
-let UKcardnumb;
-let UKcnv;
-let UKexpmonth;
-let UKexpyear;
-let desiredSize;
+let autofill = {
+	UKname: 			null,
+	UKemail: 			null,
+	UKtel: 				null,
+	UKaddr1: 			null,
+	UKaddr2: 			null,
+	UKaddr3: 			null,
+	UKcity: 			null,
+	UKpostcode: 	null,
+	UKcountry: 		null,
+	UKcardtype:		null,
+	UKcardnumb:		null,
+	UKcnv: 				null,
+	UKexpmonth:		null,
+	UKexpyear:		null,
+	desiredSize:	null
+};
 
 // Go to checkout page.
-function goToCart() {
-    window.location.href="http://www.supremenewyork.com/checkout";
+const goToCart = () => {
+	window.location.href="http://www.supremenewyork.com/checkout";
 	console.log("    ✔ Opened checkout page");
 	console.log("---------------------------------------------------------");
 	console.log("---THIS CONSOLE MAY NOT SHOW ACTIVITY ON CHECKOUT PAGE---");
 	console.log("---------------------------------------------------------");
 	clearInterval(checkInCart);
 	clearInterval(setSize);
-}
+};
 
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
+// Fill out form and replace captcha response.
+const fillCheckoutForm = () => {
+	document.getElementById("order_billing_name").value				= UKname; 
+	document.getElementById("order_email").value							= UKemail;
+	document.getElementById("order_tel").value								= UKtel;
+	document.getElementById("bo").value												= UKaddr1;
+	document.getElementById("oba3").value											= UKaddr2;
+	document.getElementById("order_billing_address_3").value 	= UKaddr3;
+	document.getElementById("order_billing_city").value				= UKcity;
+	document.getElementById("order_billing_zip").value				= UKpostcode; 
+	document.getElementById("order_billing_country").value		= UKcountry;
+	console.log("    ✔ Filled out billing details");
+
+	document.getElementById("credit_card_type").value					= UKcardtype;
+  console.log("    ✔ Selected payment type");
+	if (UKcardtype != "paypal") {
+		document.getElementById("cnb").value										= UKcardnumb;
+		document.getElementById("vval").value										= UKcnv;
+		document.getElementById("credit_card_month").value			= UKexpmonth;
+		document.getElementById("credit_card_year").value				= UKexpyear;
+		console.log("    ✔ Filled out credit card details");
+	}
+
+	document.getElementById("order_terms").checked = true;
+	console.log("    ✔ Terms Accepted");
+};
+
 
 // Retrieves user data and starts script.
-function onGot(item) {
+const onReceiveConfig = (config) => {
 	console.log("Getting user details.");
-	UKname 		=	item.order_billing_name;
-	console.log("    ✔ Name = " + item.order_billing_name);
-	UKemail 	=	item.order_email;
-	UKtel 		=	item.order_tel;
-	UKaddr1		=	item.bo;
-	UKaddr2		=	item.oba3;
-	UKaddr3		=	item.order_billing_address_3;
-	UKcity 		=	item.order_billing_city;
-	UKpostcode 	= 	item.order_billing_zip;
-	UKcountry 	= 	item.order_billing_country;
-	UKcardtype 	=	item.credit_card_type;
-	UKcardnumb 	= 	item.cnb;
-	UKcnv 		= 	item.vval;
-	UKexpmonth 	= 	item.credit_card_month;
-	UKexpyear 	= 	item.credit_card_year;
-	desiredSize = 	item.size;
+	autofill.UKname				=	config.order_billing_name;
+	console.log("    ✔ Name = " + config.order_billing_name);
+	autofill.UKemail 			=	config.order_email;
+	autofill.UKtel				=	config.order_tel;
+	autofill.UKaddr1			=	config.bo;
+	autofill.UKaddr2			=	config.oba3;
+	autofill.UKaddr3			=	config.order_billing_address_3;
+	autofill.UKcity				=	config.order_billing_city;
+	autofill.UKpostcode		= config.order_billing_zip;
+	autofill.UKcountry		= config.order_billing_country;
+	autofill.UKcardtype		=	config.credit_card_type;
+	autofill.UKcardnumb		= config.cnb;
+	autofill.UKcnv				= config.vval;
+	autofill.UKexpmonth		= config.credit_card_month;
+	autofill.UKexpyear		= config.credit_card_year;
+	autofill.desiredSize	= config.size;
 	console.log("    ✔ Obtained user details");
 
-	let pathname 		= window.location.pathname;
-	let pathname_first 	= pathname.substring(0,5);
-	let pathname_second = pathname.substring(0,9);
-
+	// If we are on a product page.
 	if (pathname != "/checkout" && pathname.length > 25) {	// If we are on product page.
 		console.log("[STATUS]: On product page.");
 		console.log("Attemping to set size to " + desiredSize);
-		let add_to_cart = setInterval( function() {
+		const add_to_cart = setInterval(() => {
+			// If we have the item in our cart.
 			if (document.getElementById("cart-remove")) {
 				console.log("    ✔  ADDED TO CART");
 				console.log("Attempting to visit checkout page...");
 				clearInterval(add_to_cart);
 				goToCart();
 			}
-			let sizeList = document.getElementById("size");
-			let currentSize = sizeList.options[sizeList.selectedIndex].textContent;
-			let currentSizeId = sizeList.options[sizeList.selectedIndex].value;
+
+			// Set the Size & Add to Cart.
+			let sizeList			= document.getElementById("size");
+			let currentSize		= sizeList.options[sizeList.selectedIndex].textContent;
+			let currentSizeId	= sizeList.options[sizeList.selectedIndex].value;
 			console.log("Current Size = " + currentSize +". Desired Size = " + desiredSize);
 			if (currentSize != desiredSize) {
 				sizeList.value = parseInt(currentSizeId) + 1;
 			}
 			if (currentSize == desiredSize) {
 				console.log("Attempting to ADD-TO-CART.");
-				document.getElementsByTagName('input')[2].click();
+				document.getElementsByTagName("input")[2].click();
 				console.log("Clicked ADD-TO-CART");
 			}
 		}, 250);
@@ -92,10 +118,10 @@ function onGot(item) {
 	else if (pathname == "/checkout") {
 		console.log("[STATUS]: On checkout page");
 		console.log("Filling out details.");
-		fillForm();
+		fillCheckoutForm();
 		console.log("   ___ _  _ ___ ___ _  _____  _   _ _____   _  _  _____      __");
 		console.log("  / __| || | __/ __| |/ / _ \\| | | |_   _| | \\| |/ _ \\ \\    / /");
-		console.log(" | (__| __ | _| (__| ' < (_) | |_| | | |   | .` | (_) \\ \\/\\/ / ");
+		console.log(" | (__| __ | _| (__| | < (_) | |_| | | |   | .` | (_) \\ \\/\\/ / ");
 		console.log("  \\___|_||_|___\\___|_|\\_\\___/ \\___/  |_|   |_|\\_|\\___/ \\_/\\_/");
 	}
 	else {
@@ -103,63 +129,20 @@ function onGot(item) {
 		console.log("         ¬No work to do.");
 	}
 
-}
+};
 
-let getting = chrome.storage.local.get(
-	[
-	"order_billing_name",
-	"order_email",
-	"order_tel",
-	"bo",
-	"oba3",
-	"order_billing_address_3",
-	"order_billing_city",
-	"order_billing_zip",
-	"order_billing_country",
-	"credit_card_type",
-	"cnb",
-	"credit_card_month",
-	"credit_card_year",
-	"vval",
-	"size",
-	"mode",
-	"man_token"
-	]
-);
-getting.then(onGot, onError);
-
-
-let check_page = setInterval( function(){
+// Checking to see if we are on a product page.
+const check_page = setInterval(() => {
 	if (document.getElementById("size")) {
-		getting.then(onGot, onError);
+		chrome.storage.local.get(["autofill"])
+		.then(onReceiveConfig)
+		.catch(err => console.error(err));
+
 		clearInterval(check_page);
 	}
 }, 250);
 
-
-// Fill out form and replace captcha response.
-function fillForm() {
-	document.getElementById("order_billing_name").value = UKname; 
-	document.getElementById("order_email").value = UKemail;
-	document.getElementById("order_tel").value = UKtel;
-	document.getElementById("bo").value = UKaddr1;
-	document.getElementById("oba3").value = UKaddr2;
-	document.getElementById("order_billing_address_3").value = UKaddr3;
-	document.getElementById("order_billing_city").value = UKcity;
-	document.getElementById("order_billing_zip").value = UKpostcode; 
-	document.getElementById("order_billing_country").value = UKcountry;
-	console.log("    ✔ Filled out billing details");
-
-    document.getElementById("credit_card_type").value = UKcardtype;
-    console.log("    ✔ Selected payment type");
-	if (UKcardtype != "paypal") {
-        document.getElementById("cnb").value = UKcardnumb;
-        document.getElementById("vval").value = UKcnv;
-        document.getElementById("credit_card_month").value = UKexpmonth;
-        document.getElementById("credit_card_year").value = UKexpyear;
-        console.log("    ✔ Filled out credit card details");
-	}
-
-	document.getElementById("order_terms").checked = true;
-	console.log("    ✔ Terms Accepted");
-}
+/* Attempt to pull in autofill. */
+chrome.storage.local.get(["autofill"])
+.then(onReceiveConfig)
+.catch(err => console.error(err));
